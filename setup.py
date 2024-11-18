22723c2eed59b8962391a07d717086c87a385ebc
taabata <57796911+taabata@users.noreply.github.com>
2024-11-18 03:37:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
-import shutil
-
+import shutil, argparse,os,re
 shutil.copyfile('latent_preview.py', '../../latent_preview.py')
+newlines = '''
+parser.add_argument("--canvasport", default="5000")
+'''
+footer = '''
+
+os.environ["LISTEN"] = args.listen
+os.environ["CANVASPORT"] = args.canvasport
+'''
+with open('../../comfy/cli_args.py',"r") as file:
+    lines = file.read()
+
+x = re.search("argparse.ArgumentParser\(\)",lines).span()[1]
+fp1 = lines[:x]
+fp2 = lines[x:]
+newtext = fp1+"\n"+newlines+"\n"+fp2+footer
+
+with open('../../comfy/cli_args.py',"w") as file:
+    file.writelines(newtext)
